9e42ce703be2e31b58616bb93642936e3acc6176
Vinicius <vinioliveira015@gmail.com>
2020-09-25 18:24:50 -0300

    Its a implementation of the flux controller "while" in java language.

---
+++ ControleFluxoWhile.java
@@ -0,0 +1,15 @@
+package lab4AJ1;
+
+
+class ControleFluxoWhile {
+
+	public static void main(String[] args) {
+	
+	for(int i = 70; i <= 90; i++)  
+	{  
+	System.out.println(" The ASCII value of " + (char)i);  
+	}  
+	
+  	
+  }
+}
