4eda506693c7d583427db094547987824b6865d2
Vinicius <vinioliveira015@gmail.com>
2020-09-25 18:20:27 -0300

    This is a little implementation of the variables in java language.

---
+++ DeclaracaoVariaveis1.java
@@ -0,0 +1,17 @@
+package lab2AJ1;
+
+public class DeclaracaoVariaveis {
+
+	public static void main(String[] args) {
+
+		int idade = 19;
+		String nome = "Rosa", RG = "15875214-X";
+		char sexo = 'm';
+		double salario = 14500.00;
+		
+		System.out.println("Senhor " + nome +", de " + idade + " anos portador(a) do rg " + RG + "do sexo" + sexo +
+				"estah registrado com o salario: " + salario);
+		
+	}
+
+}
